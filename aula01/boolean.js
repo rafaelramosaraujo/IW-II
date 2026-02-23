10119ac9045ec7022d9dc030631788f1bcd91b9d
Rafael Ramos Araujo <159478325+rafaelramosaraujo@users.noreply.github.com>
2026-02-23 08:49:57 -0300

    Add files via upload

---
+++ aula01/boolean.js
@@ -0,0 +1,10 @@
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+console.log(int1);
+console.log(int2);
---
+++ aula01/variaveis.js
@@ -0,0 +1,13 @@
+var variavelInt = 10;
+var variavelString = "Olá, Mundo!";
+var variavelString2 = 'Teste com aspas simples'
+var variavelBoolean = false;
+var variavelNull = null;
+var variavelIndefinida;
+
+console.log(variavelInt)
+console.log(variavelString)
+console.log(variavelString2)
+console.log(variavelBoolean)
+console.log(variavelNull)
+console.log(variavelIndefinida)
